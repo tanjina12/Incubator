f8bbaa5e029c152716d65436d023cc3d6d418e0f
sangamm <sangamgupta10@gmail.com>
2018-05-22 20:46:22 +0200

    Changed ICC analysis to amandroid

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,118 +1,60 @@
 import com.natpryce.konfig.ConfigurationProperties
-import config.incubator
-import soot.Scene
-import soot.jimple.infoflow.android.SetupApplication
-import soot.jimple.infoflow.android.manifest.ProcessManifest
-import soot.jimple.toolkits.callgraph.CallGraph
-import soot.util.dot.DotGraph
+import hu.ssh.progressbar.console.ConsoleProgressBar
+import org.argus.amandroid.alir.componentSummary.ApkYard
+import org.argus.amandroid.core.AndroidGlobalConfig
+import org.argus.amandroid.core.ApkGlobal
+import org.argus.amandroid.core.decompile.DecompileLayout
+import org.argus.amandroid.core.decompile.DecompileLevel
+import org.argus.amandroid.core.decompile.DecompileStrategy
+import org.argus.amandroid.core.decompile.DecompilerSettings
+import org.argus.jawa.alir.pta.suspark.InterProceduralSuperSpark
+import org.argus.jawa.core.DefaultLibraryAPISummary
+import org.argus.jawa.core.MsgLevel
+import org.argus.jawa.core.PrintReporter
+import org.argus.jawa.core.Signature
+import org.argus.jawa.core.util.FileUtil
+import scala.Option
+import scala.collection.immutable.Seq
+import scala.collection.immutable.Set
 import java.io.File
-import java.nio.file.StandardOpenOption
 
 
 fun main(args: Array<String>) {
     println("Starting incubator")
     val config = ConfigurationProperties.fromFile(File("incubator.properties"))
 
-    val androidSdkPath = config[incubator.androidSdkPath]
 
-    File(config[incubator.apk]).walk().forEach {
-        if (it.extension == "apk"){
+//    val apkUri = FileUtil.toUri("app.apk")
+//    val outputUri = FileUtil.toUri("./output")
+//    val reporter = PrintReporter(MsgLevel.ERROR())
+//    val yard = ApkYard(reporter)
+//    val layout = DecompileLayout(outputUri, true, "src", "lib", true)
+//    val consoleBar = ConsoleProgressBar.on(System.out).withFormat("[:bar] :percent% :elapsed ETA: :eta")
+//    val strategy = DecompileStrategy(layout, DefaultLibraryAPISummary(AndroidGlobalConfig.settings().third_party_lib_file()), DecompileLevel.UNTYPED(), DecompileLevel.SIGNATURE(), true)
+//
+//    val settings = DecompilerSettings(false, true, strategy, reporter, Option.empty(), 15, consoleBar)
+//
+//    val apk : ApkGlobal = yard.loadApk(apkUri, settings, true, true, false)
+//
+//
+//    val name = apk.model().appName
+//    println("Name of app $name")
+//    println("Activities ${apk.model().activities}")
+//    println("Appname ${apk.model().appName}")
+//    println("CB methods ${apk.model().callbackMethods}")
+//    apk.model().components.foreach{ svc ->
+//        println(svc)
+//        val clz = apk.getClassOrResolve(svc)
+//        val eps = clz.declaredMethods
+//        val spark = InterProceduralSuperSpark(apk)
+//        val l : Set<Signature>
+//
+//        l = eps.map()
+//        val idfg = spark.build(eps.map(getSignature))
+//        println(idfg.ptaresult())
+//    }
 
-            val manifest = ProcessManifest(it)
-            val packageName = manifest.packageName
-            println(it)
-
-            val ic3Dir = File("${config[incubator.apkIC3File]}/$packageName/")
-
-            if (ic3Dir.exists()){
-                val ic3Result = ic3Dir.listFiles().first()
-                println(ic3Result)
-
-                try {
-                    run(androidSdkPath, it.toString(), ic3Result.toString(), packageName)
-                }catch (e : Exception){
-                    println(e.printStackTrace())
-                    println("Failed $packageName skipping..")
-                    val errorFile = File("errors.txt")
-                    errorFile.writeText("Failed $packageName skipping..")
-                    errorFile.writeText(e.printStackTrace().toString())
-                }
-            }
-        }
-    }
-
-}
-
-fun run(androidSdkPath: String, apk : String, IC3Model : String, packageName: String){
-    val app = SetupApplication(androidSdkPath, apk)
-    app.config.isTaintAnalysisEnabled = false
-    app.config.mergeDexFiles = true
-    app.config.iccConfig.iccModel = IC3Model
-
-    app.constructCallgraph()
+    Argus().run()
 
 
-    val callGraph = Scene.v().callGraph
-
-    printFullTree(callGraph, packageName)
-    printActivityOnly(callGraph, packageName)
-    printAppTree(callGraph, packageName)
-}
-
-fun printFullTree(cg: CallGraph, packageName: String) {
-    val dg = DotGraph("Call Graph")
-    val edges = cg.listener()
-    while (edges.hasNext()) {
-        val e = edges.next()
-        val src = e.src.method()
-        val tgt = e.tgt.method()
-        dg.drawEdge(src.signature, tgt.signature)
-    }
-
-    dg.plot("./output/${packageName}_fullgraph.dot")
-}
-
-fun printAppTree(cg: CallGraph, packageName: String) {
-    val dg = DotGraph("Call Graph")
-    val edges = cg.listener()
-    while (edges.hasNext()) {
-        val e = edges.next()
-        val src = e.src.method()
-        val tgt = e.tgt.method()
-        if (src.declaringClass.packageName == packageName || src.declaringClass.name == "IpcSC"  ) {
-            if (tgt.declaringClass.name == "IpcSC" || tgt.declaringClass.packageName == packageName){
-                dg.drawEdge(src.signature, tgt.signature)
-            }
-        }
-    }
-
-    dg.plot("./output/${packageName}_appgraph.dot")
-}
-
-fun printActivityOnly(cg: CallGraph, packageName: String) {
-    val links = mutableSetOf<Pair<String, String>>()
-    val dg = DotGraph("Call Graph")
-    val edges = cg.listener()
-    while (edges.hasNext()) {
-        val e = edges.next()
-        val src = e.src.method()
-        val tgt = e.tgt.method()
-
-        if (src.declaringClass.name == "IpcSC") {
-
-            val edgesOutSources = cg.edgesInto(src)
-            while (edgesOutSources.hasNext()){
-                val edge = edgesOutSources.next()
-                if (edge.tgt().declaringClass.name == "IpcSC"){
-                    if(links.add(Pair(edge.src().signature, tgt.signature))){
-                        val dgEdge = dg.drawEdge(edge.src().declaringClass.name, tgt.declaringClass.name)
-//                        val dgEdge = dg.drawEdge(edge.src().signature, tgt.signature)
-                        dgEdge.setLabel(edge.src().name)
-                    }
-                }
-            }
-        }
-
-        dg.plot("./output/${packageName}_actvitygraph.dot")
-    }
 }
---
+++ src/main/scala/Argus.scala
@@ -0,0 +1,214 @@
+import java.io.{File, PrintWriter}
+import java.util.concurrent.TimeUnit
+
+import hu.ssh.progressbar.console.ConsoleProgressBar
+import org.argus.amandroid.alir.componentSummary.ApkYard
+import org.argus.amandroid.alir.dataRecorder.{DataCollector, MetricRepo}
+import org.argus.amandroid.alir.pta.model.AndroidModelCallHandler
+import org.argus.amandroid.alir.pta.reachingFactsAnalysis.{AndroidReachingFactsAnalysis, AndroidReachingFactsAnalysisConfig, IntentHelper}
+import org.argus.amandroid.alir.pta.summaryBasedAnalysis.AndroidSummaryProvider
+import org.argus.amandroid.core.decompile.{DecompileLayout, DecompileStrategy, DecompilerSettings}
+import org.argus.amandroid.core.model.Intent
+import org.argus.amandroid.core.{AndroidConstants, AndroidGlobalConfig, ApkGlobal}
+import org.argus.amandroid.summary.wu.IntentWu
+import org.argus.jawa.alir.Context
+import org.argus.jawa.alir.cfg.{ICFGNode, InterProceduralControlFlowGraph}
+import org.argus.jawa.alir.cg.CallGraph
+import org.argus.jawa.alir.dda.InterProceduralDataDependenceAnalysis
+import org.argus.jawa.alir.pta.suspark.InterProceduralSuperSpark
+import org.argus.jawa.alir.pta.{PTAResult, PTASlot}
+import org.argus.jawa.alir.reachability.SignatureBasedCallGraph
+import org.argus.jawa.core._
+import org.argus.jawa.core.util._
+import org.argus.jawa.summary.wu.{PTStore, PTSummary, WorkUnit}
+import org.argus.jawa.summary.{BottomUpSummaryGenerator, SummaryManager}
+
+import scala.concurrent.duration.FiniteDuration
+import scala.util.matching.Regex
+
+class Argus {
+
+  def run(): Unit = {
+    println("Running argus in Scala")
+//    val apkUri = FileUtil.toUri("app-debug.apk")
+    val apkUri = FileUtil.toUri("aarddict.android_26.apk")
+    val outputUri = FileUtil.toUri("./output")
+    val reporter = new PrintReporter(MsgLevel.ERROR)
+    val yard = new ApkYard(reporter)
+    val layout = DecompileLayout(outputUri, true, "src", "lib", true)
+    val strategy = DecompileStrategy(layout, new DefaultLibraryAPISummary(AndroidGlobalConfig.settings.third_party_lib_file))
+
+    val settings = DecompilerSettings(false, true, strategy, reporter)
+
+    val apk: ApkGlobal = yard.loadApk(apkUri, settings, true, true, false)
+
+    //    Spark(apk)
+    //    Signature(apk)
+    //    Facts(apk)
+    Intent(apk)
+  }
+
+
+  def Spark(apk: ApkGlobal): Unit = {
+    var entryPoints = apk.getEntryPoints(AndroidConstants.MAINCOMP_ENV) // Exposed components
+    val spark = new InterProceduralSuperSpark(apk)
+    val idfg = spark.build(entryPoints.map(_.getSignature))
+    val icfg = idfg.icfg
+    val callGraph = icfg.getCallGraph
+
+    icfg.toDot(new PrintWriter(System.out))
+
+
+    val x = idfg.ptaresult.pointsToMap
+  }
+
+  def Signature(apk: ApkGlobal): Unit = {
+    var entryPoints = apk.getEntryPoints(AndroidConstants.MAINCOMP_ENV)
+    val signature = SignatureBasedCallGraph(apk, entryPoints.map(_.getSignature), None)
+    val callMap = signature.getCallMap
+  }
+
+  def Facts(apk: ApkGlobal): Unit = {
+    val icfg = new InterProceduralControlFlowGraph[ICFGNode]
+    val ptaresult = new PTAResult
+    val sp = new AndroidSummaryProvider(apk)
+
+    AndroidReachingFactsAnalysisConfig.resolve_icc = true
+    AndroidReachingFactsAnalysisConfig.resolve_static_init = false
+    AndroidReachingFactsAnalysisConfig.parallel = false
+
+    val analysis = new AndroidReachingFactsAnalysis(apk, icfg, ptaresult,
+      new AndroidModelCallHandler,
+      sp.getSummaryManager,
+      new ClassLoadManager,
+      AndroidReachingFactsAnalysisConfig.resolve_static_init,
+      timeout = Some(new MyTimeout(FiniteDuration(5, TimeUnit.MINUTES)))
+    )
+
+    var entryPoints = apk.getEntryPoints(AndroidConstants.MAINCOMP_ENV)
+
+    val entryPoint: JawaMethod = entryPoints.head
+    val idfg = analysis.build(entryPoint, initContext = new Context(apk.nameUri))
+    val iddResult = InterProceduralDataDependenceAnalysis(apk, idfg)
+    println(iddResult)
+
+    idfg.icfg.toDot(new PrintWriter(System.out))
+  }
+
+  def Intent(apk: ApkGlobal): Unit = {
+    val handler: AndroidModelCallHandler = new AndroidModelCallHandler
+    val sm: SummaryManager = new AndroidSummaryProvider(apk).getSummaryManager
+    val analysis = new BottomUpSummaryGenerator[Global](apk, sm, handler,
+      PTSummary(_, _),
+      ConsoleProgressBar.on(System.out).withFormat("[:bar] :percent% :elapsed Left: :remain"))
+    val store: PTStore = new PTStore
+    val sigs: ISet[Signature] = apk.model.getComponentInfos.flatMap(apk.getEntryPoints)
+    val cg = SignatureBasedCallGraph(apk, sigs, None)
+    val orderedWUs: IList[WorkUnit[Global]] = cg.topologicalSort(true).map { sig =>
+      val method = apk.getMethodOrResolve(sig).getOrElse(throw new RuntimeException("Method does not exist: " + sig))
+      new IntentWu(apk, method, sm, handler, store, "intent")
+    }
+
+    analysis.build(orderedWUs)
+    val candidate = store.getPropertyOrElse[MSet[(Context, PTASlot)]]("intent", msetEmpty)
+    val intents: MSet[Intent] = msetEmpty
+
+
+    val signature: MMap[Signature, String] = mmapEmpty
+    candidate.foreach { case (ctx, s) =>
+      val intentInss = store.resolved.pointsToSet(ctx, s)
+      val intent = IntentHelper.getIntentContents(store.resolved, intentInss, ctx)
+      intents ++= intent
+      println(s"${ctx.getMethodSig.methodName} calls Intent:")
+      println(intent)
+      println()
+      if (intent.head.componentNames.nonEmpty) {
+        signature.put(ctx.getMethodSig, intent.head.componentNames.head)
+      } else {
+        println(s"NO component link. Its likely ann action ${intent}")
+      }
+    }
+
+    signature.foreach { s =>
+      val sig = getSignature(sigs, s._2)
+      cg.addCall(s._1, sig)
+    }
+
+//    generateDotFile(cg, apk)
+    generateDotFileSmall(cg, apk)
+  }
+
+  def getSignature(sigs: ISet[Signature], className: String): Signature = {
+    val targetSig = sigs.filter(p => p.methodName == "onCreate" && p.getClassName == className)
+    if (targetSig.nonEmpty) {
+      if (targetSig.size == 1) {
+        println("onCreate found")
+        return targetSig.head
+      } else {
+        println("More than one 1 onCreate found!")
+      }
+    }
+    println(s"No onCreate method found! $className")
+    new Signature(s"${className.replace(".", "/")};.dummyInit:()V")
+  }
+
+  def generateDotFile(cg: CallGraph, apk: ApkGlobal): Unit = {
+    val t: MList[(String, String)] = mlistEmpty
+
+    cg.getCallMap.foreach { c =>
+      t += ((c._1.getClassName , c._1.toString()))
+      c._2.foreach { p =>
+        if (c._1.getClassName != p.getClassName) {
+          if (p.classTyp.getPackageName == apk.model.getPackageName){
+            t += ((c._1.toString(), p.signature.toString))
+          }
+        }
+      }
+    }
+
+    val writer = new PrintWriter(new File("data.dot"))
+
+    println("digraph G {")
+    writer.write("digraph G {")
+    t.foreach { x =>
+      println("\"" + x._1 + "\" -> \"" + x._2 + "\"")
+      writer.write("\"" + x._1 + "\" -> \"" + x._2 + "\"")
+    }
+    println("}")
+    writer.write("}")
+    writer.close()
+  }
+
+  def generateDotFileSmall(cg: CallGraph, apk: ApkGlobal): Unit = {
+    val t: MSet[(String, String, String)] = msetEmpty
+
+    cg.getCallMap.foreach { c =>
+      c._2.foreach { p =>
+        val source = c._1.getClassName.replaceAll("[$].*", "")
+        val target = p.getClassName.replaceFirst("[$].*", "")
+        if (source != target) {
+          if (p.classTyp.getPackageName == apk.model.getPackageName || p.methodName == "dummyInit" ){
+            t += ((source, target, c._1.methodName))
+          }
+        }
+      }
+    }
+
+    val writer = new PrintWriter(new File("data.dot"))
+
+    println("digraph G {")
+    writer.write("digraph G {")
+    t.foreach { x =>
+      println("\"" + x._1 + "\" -> \"" + x._2 + "\"[label="+x._3+"]")
+      writer.write("\"" + x._1 + "\" -> \"" + x._2 + "\"[label="+x._3+"]")
+    }
+    apk.model.getActivities.foreach{
+      x => println("\""+x.name+"\"")
+        writer.write("\""+x.name+"\"")
+    }
+    println("}")
+    writer.write("}")
+    writer.close()
+  }
+
+}
