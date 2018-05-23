04d8a2aa3d594283ad014fd9ca7ae8ec6500bfd9
Sangam <sangamgupta10@gmail.com>
2018-05-23 02:15:52 +0200

    Improve code

--- src/main/scala/Argus.scala
+++ src/main/scala/Argus.scala
@@ -1,8 +1,10 @@
 import java.io.{File, PrintWriter}
+import java.nio.channels.Channels
 import java.util.concurrent.TimeUnit
 
 import hu.ssh.progressbar.console.ConsoleProgressBar
-import org.argus.amandroid.alir.componentSummary.ApkYard
+import org.argus.amandroid.alir.componentSummary.ComponentSummaryTable.CHANNELS
+import org.argus.amandroid.alir.componentSummary._
 import org.argus.amandroid.alir.dataRecorder.{DataCollector, MetricRepo}
 import org.argus.amandroid.alir.pta.model.AndroidModelCallHandler
 import org.argus.amandroid.alir.pta.reachingFactsAnalysis.{AndroidReachingFactsAnalysis, AndroidReachingFactsAnalysisConfig, IntentHelper}
@@ -10,6 +12,7 @@ import org.argus.amandroid.alir.pta.summaryBasedAnalysis.AndroidSummaryProvider
 import org.argus.amandroid.core.decompile.{DecompileLayout, DecompileStrategy, DecompilerSettings}
 import org.argus.amandroid.core.model.Intent
 import org.argus.amandroid.core.{AndroidConstants, AndroidGlobalConfig, ApkGlobal}
+import org.argus.amandroid.plugin.communication.CommunicationSourceAndSinkManager
 import org.argus.amandroid.summary.wu.IntentWu
 import org.argus.jawa.alir.Context
 import org.argus.jawa.alir.cfg.{ICFGNode, InterProceduralControlFlowGraph}
@@ -19,7 +22,7 @@ import org.argus.jawa.alir.pta.suspark.InterProceduralSuperSpark
 import org.argus.jawa.alir.pta.{PTAResult, PTASlot}
 import org.argus.jawa.alir.reachability.SignatureBasedCallGraph
 import org.argus.jawa.core._
-import org.argus.jawa.core.util._
+import org.argus.jawa.core.util.{MList, _}
 import org.argus.jawa.summary.wu.{PTStore, PTSummary, WorkUnit}
 import org.argus.jawa.summary.{BottomUpSummaryGenerator, SummaryManager}
 
@@ -31,7 +34,10 @@ class Argus {
   def run(): Unit = {
     println("Running argus in Scala")
 //    val apkUri = FileUtil.toUri("app-debug.apk")
-    val apkUri = FileUtil.toUri("aarddict.android_26.apk")
+    //        val apkUri = FileUtil.toUri("aarddict.android_26.apk")
+    //    val apkUri = FileUtil.toUri("ab.nwdesings.it.autocentribalduina.apk")
+            val apkUri = FileUtil.toUri("ab.java.programming.apk")
+    //        val apkUri = FileUtil.toUri("abid.hafedh.health.babygrowthguide.apk")
     val outputUri = FileUtil.toUri("./output")
     val reporter = new PrintReporter(MsgLevel.ERROR)
     val yard = new ApkYard(reporter)
@@ -44,8 +50,9 @@ class Argus {
 
     //    Spark(apk)
     //    Signature(apk)
-    //    Facts(apk)
-    Intent(apk)
+    //        Facts(apk)
+//        Intent(apk)
+    componentBasedGraph(apk, yard)
   }
 
 
@@ -111,21 +118,21 @@ class Argus {
 
     analysis.build(orderedWUs)
     val candidate = store.getPropertyOrElse[MSet[(Context, PTASlot)]]("intent", msetEmpty)
-    val intents: MSet[Intent] = msetEmpty
+    val intents: MSet[(Intent, Signature)] = msetEmpty
 
 
     val signature: MMap[Signature, String] = mmapEmpty
     candidate.foreach { case (ctx, s) =>
       val intentInss = store.resolved.pointsToSet(ctx, s)
       val intent = IntentHelper.getIntentContents(store.resolved, intentInss, ctx)
-      intents ++= intent
       println(s"${ctx.getMethodSig.methodName} calls Intent:")
       println(intent)
       println()
       if (intent.head.componentNames.nonEmpty) {
         signature.put(ctx.getMethodSig, intent.head.componentNames.head)
+        intents.add((intent.head, ctx.getMethodSig))
       } else {
-        println(s"NO component link. Its likely ann action ${intent}")
+        println(s"NO component link. Its likely an action ${intent}")
       }
     }
 
@@ -134,8 +141,9 @@ class Argus {
       cg.addCall(s._1, sig)
     }
 
-//    generateDotFile(cg, apk)
-    generateDotFileSmall(cg, apk)
+    //    buildFullAppGraph(cg, apk)
+    //    buildSimplifiedAppGraph(cg, apk)
+    buildActivityGraph(intents, apk)
   }
 
   def getSignature(sigs: ISet[Signature], className: String): Signature = {
@@ -152,14 +160,40 @@ class Argus {
     new Signature(s"${className.replace(".", "/")};.dummyInit:()V")
   }
 
-  def generateDotFile(cg: CallGraph, apk: ApkGlobal): Unit = {
+
+  def componentBasedGraph(apk: ApkGlobal, yard: ApkYard): Unit = {
+    ComponentBasedAnalysis.prepare(Set(apk))(FiniteDuration(5, TimeUnit.MINUTES))
+    val cba = new ComponentBasedAnalysis(yard)
+    cba.phase1(Set(apk))
+    val iddResult = cba.phase2(Set(apk))
+
+    val l: MSet[(ICFGNode, IntentCaller)] = msetEmpty
+
+    apk.getSummaryTables.foreach { st =>
+      val table: ICC_Summary = st._2.get(CHANNELS.ICC)
+      table.asCaller.foreach { x =>
+        val intent: IntentCaller = x._2.asInstanceOf[IntentCaller]
+        if (intent.intent.componentNames.nonEmpty) {
+          l.add((x._1, intent))
+        } else {
+          println(s"NO component link. Its likely an action ${intent}")
+        }
+      }
+    }
+
+    apk.model.getCallbackMethods
+
+    buildActivityGraph(l.map(x => (x._2.intent, x._1.getOwner)), apk)
+  }
+
+  def buildFullAppGraph(cg: CallGraph, apk: ApkGlobal): Unit = {
     val t: MList[(String, String)] = mlistEmpty
 
     cg.getCallMap.foreach { c =>
-      t += ((c._1.getClassName , c._1.toString()))
+      t += ((c._1.getClassName, c._1.toString()))
       c._2.foreach { p =>
         if (c._1.getClassName != p.getClassName) {
-          if (p.classTyp.getPackageName == apk.model.getPackageName){
+          if (p.classTyp.getPackageName == apk.model.getPackageName) {
             t += ((c._1.toString(), p.signature.toString))
           }
         }
@@ -179,7 +213,7 @@ class Argus {
     writer.close()
   }
 
-  def generateDotFileSmall(cg: CallGraph, apk: ApkGlobal): Unit = {
+  def buildSimplifiedAppGraph(cg: CallGraph, apk: ApkGlobal): Unit = {
     val t: MSet[(String, String, String)] = msetEmpty
 
     cg.getCallMap.foreach { c =>
@@ -187,7 +221,7 @@ class Argus {
         val source = c._1.getClassName.replaceAll("[$].*", "")
         val target = p.getClassName.replaceFirst("[$].*", "")
         if (source != target) {
-          if (p.classTyp.getPackageName == apk.model.getPackageName || p.methodName == "dummyInit" ){
+          if (p.classTyp.getPackageName == apk.model.getPackageName || p.methodName == "dummyInit") {
             t += ((source, target, c._1.methodName))
           }
         }
@@ -199,16 +233,45 @@ class Argus {
     println("digraph G {")
     writer.write("digraph G {")
     t.foreach { x =>
-      println("\"" + x._1 + "\" -> \"" + x._2 + "\"[label="+x._3+"]")
-      writer.write("\"" + x._1 + "\" -> \"" + x._2 + "\"[label="+x._3+"]")
+      println("\"" + x._1 + "\" -> \"" + x._2 + "\"[label=" + x._3 + "]")
+      writer.write("\"" + x._1 + "\" -> \"" + x._2 + "\"[label=" + x._3 + "]")
     }
-    apk.model.getActivities.foreach{
-      x => println("\""+x.name+"\"")
-        writer.write("\""+x.name+"\"")
+    apk.model.getActivities.foreach {
+      x =>
+        println("\"" + x.name + "\"")
+        writer.write("\"" + x.name + "\"")
     }
     println("}")
     writer.write("}")
     writer.close()
   }
 
+  def buildActivityGraph(intents: MSet[(Intent, Signature)], apk: ApkGlobal): Unit = {
+
+    val t: MSet[(String, String, String)] = msetEmpty
+
+    intents.foreach { x =>
+      val target = x._1.componentNames.head
+      val source = x._2.getClassName
+      val label = x._2.methodName
+      t.add(source, target, label)
+    }
+
+    val writer = new PrintWriter(new File("data.dot"))
+
+    println("digraph G {")
+    writer.write("digraph G {")
+    t.foreach { x =>
+      println("\"" + x._1 + "\" -> \"" + x._2 + "\"[label=" + x._3 + "]")
+      writer.write("\"" + x._1 + "\" -> \"" + x._2 + "\"[label=" + x._3 + "]")
+    }
+    apk.model.getActivities.foreach {
+      x =>
+        println("\"" + x.name + "\"")
+        writer.write("\"" + x.name + "\"")
+    }
+    println("}")
+    writer.write("}")
+    writer.close()
+  }
 }
