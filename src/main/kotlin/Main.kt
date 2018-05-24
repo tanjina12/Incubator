0408243bea4731b926d74b1e6450f3412e475669
sangamm <sangamgupta10@gmail.com>
2018-05-24 15:04:24 +0200

    Add ignore apps function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ import java.io.File
 import config.incubator
 import org.apache.commons.brut.io.FileUtils
 import java.io.FileOutputStream
+import java.io.IOException
 import java.io.PrintWriter
 
 
@@ -11,8 +12,12 @@ fun main(args: Array<String>) {
     val config = ConfigurationProperties.fromFile(File("incubator.properties"))
     val basePathResult = config[incubator.output]
     val mode = config[incubator.mode]
+    val ignoreAppsFile = config[incubator.ignoreApps]
+
+    val ignoreApps = File(ignoreAppsFile).readLines().toSet()
+
     File(config[incubator.apk]).walk().forEach {
-        if (it.extension == "apk") {
+        if (it.extension == "apk" && !ignoreApps.contains(it.toString())) {
             println(it)
 
             if (!File(basePathResult).exists()) {
@@ -21,13 +26,14 @@ fun main(args: Array<String>) {
 
             try {
                 Argus().run(it.toString(), basePathResult, mode.toString())
-                if(File("./output").isDirectory){
+                if (File("./output").isDirectory) {
                     FileUtils.deleteDirectory(File("./output"))
                 }
 
-                val writer = PrintWriter(FileOutputStream(File("succes.csv"),true))
-                writer.append(it.toString()+"\r\n")
+                val writer = PrintWriter(FileOutputStream(File("succes.csv"), true))
+                writer.append(it.toString() + "\r\n")
                 writer.close()
+            } catch (e : IOException){
             } catch (e: Exception) {
                 println(e.printStackTrace())
                 println("Failed skipping.. $it")
--- src/main/kotlin/config/Incubator.kt
+++ src/main/kotlin/config/Incubator.kt
@@ -10,4 +10,5 @@ object incubator : PropertyGroup() {
     val apk by stringType
     val output by stringType
     val mode by enumType(mapOf("ACTIVITY" to Mode.ACTIVITY, "SIMPLE" to Mode.SIMPLE, "FULL" to Mode.FULL))
+    val ignoreApps by stringType
 }
