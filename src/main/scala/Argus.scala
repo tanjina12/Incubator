6a55a20d352d8d7ed7a8a514fe14e58aea4aa30c
tanjina.islam <t.islam@student.vu.nl>
2019-06-26 21:52:31 +0200

    removed the parts that we don't need to generate

--- src/main/scala/Argus.scala
+++ src/main/scala/Argus.scala
@@ -37,36 +37,40 @@ class Argus {
     printEstimatedTimeElapsed(startTime)
 
 
-    parser.bindWidgetsToIccMethods(apk, reporter)
+    //parser.bindWidgetsToIccMethods(apk, reporter)
 
 
     println("Write graph to file")
-    val csvWriter: CsvGraphWriter = new CsvGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}.csv"))
+    // Contains Activities and Fragments, a couple of Services, Models and other elements!!
+   // val csvWriter: CsvGraphWriter = new CsvGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}.csv"))
     val xmlWriter: XmlGraphWriter = new XmlGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}.xml"))
-    val dotWriter: DotGraphWriter = new DotGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}.dot"))
+    //val dotWriter: DotGraphWriter = new DotGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}.dot"))
     val outWriter: SystemOutGraphWriter = new SystemOutGraphWriter
-    val methodWriter: MethodWriter = new MethodWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}-methods.csv"))
-    val writers = Set(csvWriter, xmlWriter, dotWriter, outWriter)
+    //val methodWriter: MethodWriter = new MethodWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}-methods.csv"))
+    //val writers = Set(csvWriter, xmlWriter, dotWriter, outWriter)
+    val writers = Set(xmlWriter, outWriter)
 
     parser.writeGraph(writers, apk)
     printEstimatedTimeElapsed(startTime)
 
-    println("Write methods")
-    parser.writeMethods(methodWriter, apk)
-    printEstimatedTimeElapsed(startTime)
-
-    println("Write transitive graph to file")
-    val tcsvWriter: CsvGraphWriter = new CsvGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}-transitive.csv"))
-    val tacsvWriter: BaseGraphWriter = new CsvGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}-act-transitive.csv"))
-    val txmlWriter: XmlGraphWriter = new XmlGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}-transitive.xml"))
-    val tdotWriter: DotGraphWriter = new DotGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}-transitive.dot"))
-    val twriters = Set(tcsvWriter, txmlWriter, tdotWriter)
-
-    val tawriters = Set(tacsvWriter)
-
-    parser.writeTransitiveGraph(twriters, apk, parser.collectComponents(apk).map(x => x.getType.jawaName))
-
-    parser.writeTransitiveGraph(tawriters, apk, apk.model.getActivities.map(x => x.jawaName))
+//    println("Write methods")
+//    parser.writeMethods(methodWriter, apk)
+//    printEstimatedTimeElapsed(startTime)
+//
+//    println("Write transitive graph to file")
+    // Contains only Activities and Fragments
+    // -act-transitive.csv file contains more entry than the other files!!
+//    val tcsvWriter: CsvGraphWriter = new CsvGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}-transitive.csv"))
+//    val tacsvWriter: BaseGraphWriter = new CsvGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}-act-transitive.csv"))
+//    val txmlWriter: XmlGraphWriter = new XmlGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}-transitive.xml"))
+//    val tdotWriter: DotGraphWriter = new DotGraphWriter(new PrintWriter(s"$baseResultPath/${apk.model.getPackageName}-transitive.dot"))
+//    val twriters = Set(tcsvWriter, txmlWriter, tdotWriter)
+//
+//    val tawriters = Set(tacsvWriter)
+//
+//    parser.writeTransitiveGraph(twriters, apk, parser.collectComponents(apk).map(x => x.getType.jawaName))
+//
+//    parser.writeTransitiveGraph(tawriters, apk, apk.model.getActivities.map(x => x.jawaName))
 
     println(s"Finished analysis for ${apk.model.getPackageName}")
   }
